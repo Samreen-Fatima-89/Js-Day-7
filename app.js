7c1f59d7144341d98d119c0aaafabb841825b114
Samreen Fatima <irfankhans786787789@gmail.com>
2024-09-17 20:36:12 +0500

    completed

---
+++ app.js
@@ -0,0 +1,26 @@
+//  
+// var skillsReq = "HTML/CSS";
+// var eduReq = "BSCS";
+// var cityReq = "karachi";
+// var skills = prompt("Enter your skills");
+// var edu = prompt("Enter your edu");
+// var city = prompt("Enter yuor city");
+// if (skills == skillsReq){
+//      if(edu == eduReq){
+//       if(city == cityReq){
+//         alert("kal sy ajao...")
+//       } else{
+//         alert("karachi m ajao...")
+//       }
+//      } else{
+//         alert("parh k ao...")
+//      }
+// } else {
+//     alert ("sikh ky ao...")
+ 
+
+
+// var studends = [["sara","saima"] , ["sareen",["farooq"]]]
+// console.log(studends[1][1][0][3]);
+
+
